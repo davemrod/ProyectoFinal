70c3ffc32a85953afbf902650974394dec306f92
David Emilio <David Emilio@192.168.0.159>
2021-06-12 04:03:37 -0500

    Se agregaron imagenes para el JFrame de habitaciones

--- src/misclases/GenerarRecibo.java
+++ src/misclases/GenerarRecibo.java
@@ -2,6 +2,7 @@
 package misclases;
 
 //import controlMySql.MySqlConn;
+import java.awt.Color;
 import java.awt.Image;
 import java.util.ArrayList;
 import javax.swing.Icon;
@@ -16,15 +17,22 @@ import misclases.Constructores;
 public class GenerarRecibo extends JFrame {
     private ImageIcon imagen;
     private Icon icono;
-    private JLabel label, jLabelHuesped;
+    private JLabel label, jLabelHuesped, jLabelCiudad, jLabelFechas, jLabelFechal, jLabelNum, jLabelTipo, jLabelOcu;
     private JButton JButtonIngresar;
     private JTextField JTextFieldUsuario;
     
-    String huesped;
+    String huesped="Hola";
+    String ciudad="Aguascalientes";
+    String costo="450000$";
+    String habi="234";
+    String fechas="23-05-2021";
+    String fechal="15-05-2021";
+    String tipo="Sencilla";
+    String ocupa="5";
     
     private ArrayList <Constructores>lista;
     
-    public GenerarRecibo(ArrayList lista) {
+    public GenerarRecibo() {
         this.lista = lista;
         initComponents(); 
         this.setLocationRelativeTo(this);
@@ -45,11 +53,40 @@ public class GenerarRecibo extends JFrame {
     private void initComponents(){
         super.setSize(750, 950);
         
-        jLabelHuesped = new JLabel(lista.get(0).getHuesped());     
-        jLabelHuesped.setBounds(10,120,300,80);
+        jLabelHuesped = new JLabel(huesped);     
+        jLabelHuesped.setBounds(175,610,300,80);
+        jLabelHuesped.setForeground(Color.RED);
         add(jLabelHuesped);
         
-  
+        jLabelCiudad = new JLabel(ciudad);     
+        jLabelCiudad.setBounds(175,630,300,80);
+        jLabelCiudad.setForeground(Color.RED);
+        add(jLabelCiudad);
+        
+        jLabelFechal = new JLabel(fechal);     
+        jLabelFechal.setBounds(217,658,300,80);
+        jLabelFechal.setForeground(Color.RED);
+        add(jLabelFechal);
+        
+        jLabelFechas = new JLabel(fechas);     
+        jLabelFechas.setBounds(217,680,300,80);
+        jLabelFechas.setForeground(Color.RED);
+        add(jLabelFechas);
+        
+        jLabelNum = new JLabel(habi);     
+        jLabelNum .setBounds(227,707,300,80);
+        jLabelNum .setForeground(Color.RED);
+        add(jLabelNum);
+        
+        jLabelTipo = new JLabel(tipo);     
+        jLabelTipo .setBounds(227,732,300,80);
+        jLabelTipo .setForeground(Color.RED);
+        add(jLabelTipo);
+        
+        jLabelOcu = new JLabel(ocupa);     
+        jLabelOcu .setBounds(235,753,300,80);
+        jLabelOcu .setForeground(Color.RED);
+        add(jLabelOcu);
 
         
          // Boton Ingresar
@@ -69,7 +106,7 @@ public class GenerarRecibo extends JFrame {
         crearFondo();       
     }
     
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
         new GenerarRecibo().setVisible(true);
-    }*/
+    }
 }
