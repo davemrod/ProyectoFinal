07485bac6ffbd58c6040c1f59cb6a7c60617a2f1
Ricardo <TheRickbro@LAPTOP-9P08FCDA>
2021-06-18 15:26:42 -0500

    Agregar dispose a galeria

--- src/misclases/Galeria.java
+++ src/misclases/Galeria.java
@@ -48,7 +48,7 @@ public class Galeria extends javax.swing.JFrame {
         jButtonStop = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(21, 19, 19));
 
